bc9e8f3b11cda4a2648b00c75c87d8062926d0c3
Zhan <zhanenev6@gmail.com>
2024-02-18 19:14:01 +0200

    Exam push

---
+++ ASP-fundamentals-exam/SeminarHub/Data/Migrations/20240218123431_Initial.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeminarHub.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seminar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Topic = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Lecturer = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    OrganizerId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DateAndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<int>(type: "int", maxLength: 180, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seminar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seminar_AspNetUsers_OrganizerId",
+                        column: x => x.OrganizerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Seminar_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SeminarParticipant",
+                columns: table => new
+                {
+                    SeminarId = table.Column<int>(type: "int", nullable: false),
+                    ParticipantId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeminarParticipant", x => new { x.SeminarId, x.ParticipantId });
+                    table.ForeignKey(
+                        name: "FK_SeminarParticipant_AspNetUsers_ParticipantId",
+                        column: x => x.ParticipantId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SeminarParticipant_Seminar_SeminarId",
+                        column: x => x.SeminarId,
+                        principalTable: "Seminar",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Technology & Innovation" },
+                    { 2, "Business & Entrepreneurship" },
+                    { 3, "Science & Research" },
+                    { 4, "Arts & Culture" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seminar_CategoryId",
+                table: "Seminar",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seminar_OrganizerId",
+                table: "Seminar",
+                column: "OrganizerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeminarParticipant_ParticipantId",
+                table: "SeminarParticipant",
+                column: "ParticipantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SeminarParticipant");
+
+            migrationBuilder.DropTable(
+                name: "Seminar");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
---
+++ ASP-fundamentals-exam/SeminarHub/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SeminarHub.Data.DataConstants;
+
+namespace SeminarHub.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
